2ddae24133c690c989eb5a33f3ea72390d6813be
Neophear <stiiggade@gmail.com>
2019-10-11 09:58:33 +0200

    Initial commit

---
+++ FolderSizes/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace FolderSizes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.fbdFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnFolderBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Location = new System.Drawing.Point(13, 16);
+            this.lblFolder.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(48, 17);
+            this.lblFolder.TabIndex = 1;
+            this.lblFolder.Text = "Folder";
+            // 
+            // fbdFolder
+            // 
+            this.fbdFolder.ShowNewFolderButton = false;
+            // 
+            // btnFolderBrowse
+            // 
+            this.btnFolderBrowse.Location = new System.Drawing.Point(256, 13);
+            this.btnFolderBrowse.Name = "btnFolderBrowse";
+            this.btnFolderBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnFolderBrowse.TabIndex = 2;
+            this.btnFolderBrowse.Text = "Browse";
+            this.btnFolderBrowse.UseVisualStyleBackColor = true;
+            this.btnFolderBrowse.Click += new System.EventHandler(this.btnFolderBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 410);
+            this.Controls.Add(this.btnFolderBrowse);
+            this.Controls.Add(this.lblFolder);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblFolder;
+        private System.Windows.Forms.FolderBrowserDialog fbdFolder;
+        private System.Windows.Forms.Button btnFolderBrowse;
+    }
+}
+
